5337a212dabcb1eab3b7a3a0b08bb91d118e6ecc
twallac10 <terence.wallace@gmail.com>
2024-02-23 05:56:31 -0600

    fixing starship

--- starship/install.sh
+++ starship/install.sh
@@ -2,5 +2,6 @@ set -e
 
 if test ! $(which starship); then
   printf "\n🚀 Installing the starship prompt\n"
-  sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+  curl -sS https://starship.rs/install.sh | sh -s -- -y
+
 fi
