183ad77983922a9c6c2aa856b682f74c0ac04a75
twallac10 <terence.wallace@gmail.com>
2024-03-22 12:33:39 -0500

    adding darwin conditional

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,9 +12,16 @@ echo "Starting installation..."
 ###
 # Install dependencies
 ###
-./bin/dotfiles install omz
+if [ "$(uname)" == "Darwin" ]; then
+    echo "Installing dependencies for macOS"
+    ./bin/dotfiles install brew
+    ./bin/dotfiles install omz
+    ./bin/dotfiles install zsh
+fi
+
+echo "Installing dependencies for Linux"
+
 ./bin/dotfiles install starship
-./bin/dotfiles install zsh
 #./bin/dotfiles install brew
 ./bin/dotfiles install vscode
 ./bin/dotfiles install git
