be67e83855a872702d185caadf10cea869e648dd
twallac10 <terence.wallace@gmail.com>
2024-03-22 12:19:45 -0500

    adding check for zsh

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,4 +1,9 @@
 set -e
 
-ln -sf "${DOTFILES_LOCATION}/zsh/.zshrc" "${HOME}/.zshrc"
-ln -sf "${DOTFILES_LOCATION}/zsh/benmatselby.zsh-theme" "${HOME}/.oh-my-zsh/custom/themes/"
+if [ -n "$ZSH_VERSION" ]; then
+    echo "Shell is zsh"
+    ln -sf "${DOTFILES_LOCATION}/zsh/.zshrc" "${HOME}/.zshrc"
+    ln -sf "${DOTFILES_LOCATION}/zsh/benmatselby.zsh-theme" "${HOME}/.oh-my-zsh/custom/themes/"
+else
+    echo "Shell is not zsh"
+fi
