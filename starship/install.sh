ee290a12037e3d7eb4f63a52c14b15bbf771ad1a
twallac10 <terence.wallace@gmail.com>
2024-03-22 12:51:39 -0500

    fixing starship

--- starship/install.sh
+++ starship/install.sh
@@ -3,5 +3,12 @@ set -e
 if test ! $(which starship); then
   printf "\n🚀 Installing the starship prompt\n"
   curl -sS https://starship.rs/install.sh | sh -s -- -y
+fi
 
+if [ "$(uname)" = "Darwin" ]; then
+  echo "Configuring zshrc for starship"
+  echo 'eval "$(starship init zsh)"' >> ~/.zshrc
+else
+  echo "Configuring bashrc for starship"
+  echo 'eval "$(starship init bash)"' >> ~/.bashrc
 fi
