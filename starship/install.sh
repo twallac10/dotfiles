6b977f1841678f3b3cd55cb400d386de40012641
twallac10 <terence.wallace@gmail.com>
2024-03-22 13:39:57 -0500

    cleaning up starship config

--- starship/install.sh
+++ starship/install.sh
@@ -1,18 +1,25 @@
 set -e
 
-if test ! $(which starship); then
+install_script_url="https://starship.rs/install.sh"
+
+if ! command -v starship >/dev/null 2>&1; then
   printf "\n🚀 Installing the starship prompt\n"
-  curl -sS https://starship.rs/install.sh | sh -s -- -y
-fi
+  curl -sS "$install_script_url" | sh -s -- -y
 
-if [ "$(uname)" = "Darwin" ]; then
-  echo "Configuring zshrc for starship"
-  echo 'eval "$(starship init zsh)"' >> ~/.zshrc
+  if [ "$(uname)" = "Darwin" ]; then
+    printf "Configuring zshrc for starship"
+    echo 'eval "$(starship init zsh)"' >> ~/.zshrc
+  else
+    printf "Configuring bashrc for starship"
+    echo 'eval "$(starship init bash)"' >> ~/.bashrc
+  fi
+
+  printf "Copying starship.toml to .config directory"
+  mkdir -p ~/.config
+  cp -r "$DOTFILES_LOCATION/starship/config" ~/.config/starship
 else
-  echo "Configuring bashrc for starship"
-  echo 'eval "$(starship init bash)"' >> ~/.bashrc
+  printf "\n🚀 Starship is already installed\n"
 fi
 
-echo "Copying starship.toml to .config directory"
-mkdir -p ~/.config
-cp $DOTFILES_LOCATION/starship/config/starship.toml ~/.config/starship.toml
+
+
--- zsh/install.sh
+++ zsh/install.sh
@@ -1,9 +1,9 @@
 set -e
 
 if [ -n "$ZSH_VERSION" ]; then
-    echo "Shell is zsh"
+    printf "Shell is zsh"
     ln -sf "${DOTFILES_LOCATION}/zsh/.zshrc" "${HOME}/.zshrc"
     ln -sf "${DOTFILES_LOCATION}/zsh/benmatselby.zsh-theme" "${HOME}/.oh-my-zsh/custom/themes/"
 else
-    echo "Shell is not zsh"
+    printf "Shell is not zsh"
 fi
